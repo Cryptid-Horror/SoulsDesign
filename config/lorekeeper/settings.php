ebf1b2ee5c230980ffcfcd0065e7889f671a6399
Cryptid-Horror <matterintothevoid@gmail.com>
2020-07-28 13:59:52 -0400

    fix for settings and css

--- config/lorekeeper/settings.php
+++ config/lorekeeper/settings.php
@@ -25,19 +25,7 @@ return [
     | site wherever the name needs to be displayed.
     |
     */
-    'site_name' => 'Lorekeeper',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Site Description
-    |--------------------------------------------------------------------------
-    |
-    | This is the description used for the site in meta tags-- previews
-    | displayed on various social media sites, discord, and the like.
-    | It is not, however, displayed on the site itself. This should be kept short and snappy!
-    |
-    */
-    'site_desc' => 'A Lorekeeper ARPG',
+    'site_name' => 'Souls-Between',
     
     /*
     |--------------------------------------------------------------------------
@@ -80,7 +68,7 @@ return [
     |
     */
     'character_codes' => '{category}-{number}',
-    'character_number_digits' => 3,
+    'character_number_digits' => 4,
     'character_pull_number' => 'all',
 
     /*
