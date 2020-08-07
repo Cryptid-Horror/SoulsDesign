af51b6615d0a83fb3457dcc6949e002d71e8ccb6
Sabriel <draginraptor@gmail.com>
2020-08-07 22:52:49 +0800

    Revert "Alt lineage tree"
    This reverts commit 9a9012a8369f989b37f8e602ed89eb88d1397d3a.

--- resources/views/character/_lineage_tree.blade.php
+++ resources/views/character/_lineage_tree.blade.php
@@ -1,4 +1,4 @@
-{{--<div>
+<div>
     <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╭⎼⎼⎼⎼⎼⎼⎼⎼⎼ SSS:</p> <p id="sssPreview" class="d-inline">{!! isset($lineage['sss']) ? $lineage['sss']->displayName : 'Unknown' !!}</p><br>
     <p class="d-inline">{!! str_repeat('&nbsp', 12) !!}╭⎼⎼⎼⎼⎼⎼⎼⎼⎼ SS:</p> <p id="ssPreview" class="d-inline">{!! isset($lineage['ss']) ? $lineage['ss']->displayName : 'Unknown' !!}</p><br>
     <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╰⎼⎼⎼⎼⎼⎼⎼⎼⎼ SSD:</p> <p id="ssdPreview" class="d-inline">{!! isset($lineage['ssd']) ? $lineage['ssd']->displayName : 'Unknown' !!}</p><br>
@@ -13,21 +13,4 @@
     <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╭⎼⎼⎼⎼⎼⎼⎼⎼⎼ DDS:</p> <p id="ddsPreview" class="d-inline">{!! isset($lineage['dds']) ? $lineage['dds']->displayName : 'Unknown' !!}</p><br>
     <p class="d-inline">{!! str_repeat('&nbsp', 12) !!}╰⎼⎼⎼⎼⎼⎼⎼⎼⎼ DD:</p> <p id="ddPreview" class="d-inline">{!! isset($lineage['dd']) ? $lineage['dd']->displayName : 'Unknown' !!}</p><br>
     <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╰⎼⎼⎼⎼⎼⎼⎼⎼⎼ DDD:</p> <p id="dddPreview" class="d-inline">{!! isset($lineage['ddd']) ? $lineage['ddd']->displayName : 'Unknown' !!}</p><br>
-</div>--}}
-
-<div>
-    <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╭⎼⎼⎼⎼⎼⎼⎼⎼⎼ SSS:</p> <p id="sssPreview" class="d-inline">{!! !is_null($lineage['sss']) ? $lineage['sss']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 12) !!}╭⎼⎼⎼⎼⎼⎼⎼⎼⎼ SS:</p> <p id="ssPreview" class="d-inline">{!! !is_null($lineage['ss']) ? $lineage['ss']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╰⎼⎼⎼⎼⎼⎼⎼⎼⎼ SSD:</p> <p id="ssdPreview" class="d-inline">{!! !is_null($lineage['ssd']) ? $lineage['ssd']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">Sire:</p> <p id="sirePreview" class="d-inline">{!! !is_null($lineage['sire']) ? $lineage['sire']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╭⎼⎼⎼⎼⎼⎼⎼⎼⎼ SDS:</p> <p id="sdsPreview" class="d-inline">{!! !is_null($lineage['sds']) ? $lineage['sds']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 12) !!}╰⎼⎼⎼⎼⎼⎼⎼⎼⎼ SD:</p> <p id="sdPreview" class="d-inline">{!! !is_null($lineage['sd']) ? $lineage['sd']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╰⎼⎼⎼⎼⎼⎼⎼⎼⎼ SDD:</p> <p id="sddPreview" class="d-inline">{!! !is_null($lineage['sdd']) ? $lineage['sdd']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╭⎼⎼⎼⎼⎼⎼⎼⎼⎼ DSS:</p> <p id="dssPreview" class="d-inline">{!! !is_null($lineage['dss']) ? $lineage['dss']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 12) !!}╭⎼⎼⎼⎼⎼⎼⎼⎼⎼ DS:</p> <p id="dsPreview" class="d-inline">{!! !is_null($lineage['ds']) ? $lineage['ds']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╰⎼⎼⎼⎼⎼⎼⎼⎼⎼ DSD:</p> <p id="dsdPreview" class="d-inline">{!! !is_null($lineage['dsd']) ? $lineage['dsd']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">Dam:</p> <p id="damPreview" class="d-inline">{!! !is_null($lineage['dam']) ? $lineage['dam']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╭⎼⎼⎼⎼⎼⎼⎼⎼⎼ DDS:</p> <p id="ddsPreview" class="d-inline">{!! !is_null($lineage['dds']) ? $lineage['dds']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 12) !!}╰⎼⎼⎼⎼⎼⎼⎼⎼⎼ DD:</p> <p id="ddPreview" class="d-inline">{!! !is_null($lineage['dd']) ? $lineage['dd']->displayName : 'Unknown' !!}</p><br>
-    <p class="d-inline">{!! str_repeat('&nbsp', 50) !!}╰⎼⎼⎼⎼⎼⎼⎼⎼⎼ DDD:</p> <p id="dddPreview" class="d-inline">{!! !is_null($lineage['ddd']) ? $lineage['ddd']->displayName : 'Unknown' !!}</p><br>
 </div>
