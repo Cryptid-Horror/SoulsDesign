70a8239ade5fc553e4b30b85ceab09ccf45f6a9e
Sabriel <draginraptor@gmail.com>
2020-08-07 23:15:28 +0800

    Revert "Fix"
    This reverts commit 3350f2b90951dc7d2caf851902b38355710a456f.

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -127,8 +127,8 @@ class HomeController extends Controller
             $characterData['free_markings'] = $character['free_markings'];
             $characterData['health_status'] = $character['health_status'];
             $characterData['ouroboros'] = $character['ouroboros_emblem'] == 1 ? 1 : null;
-            $characterData['taming'] = $character['taming'];
-            $characterData['basic_aether'] = $character['basic_aether'] == 1 ? 1 : null;
+            $characterData['taming'] = $character['taming'] == 1 ? 1 : null;
+            $characterData['basic_aether'] = $character['basic_aether'];
             $characterData['low_aether'] = $character['low_magic'];
             $characterData['high_aether'] = $character['high_magic'];
             $characterData['arena_ranking'] = $character['arena_ranking'];
