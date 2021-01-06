41e42195f55c1eea924b9b12cba756d38592bc7d
Draginraptor <draginraptor@gmail.com>
2021-01-07 02:39:34 +0800

    Quick check

--- resources/views/browse/_masterlist_content.blade.php
+++ resources/views/browse/_masterlist_content.blade.php
@@ -125,7 +125,11 @@
             @foreach($chunk as $character)
             <div class="col-md-3 col-6 text-center">
                 <div>
-                    <a href="{{ $character->url }}"><img src="{{ $character->image->thumbnailUrl }}" class="img-thumbnail" /></a>
+                    @if(isset($character->image->thumbnailUrl))
+                        <a href="{{ $character->url }}"><img src="{{ $character->image->thumbnailUrl }}" class="img-thumbnail" /></a>
+                    @else
+                        Image could not be displayed.
+                    @endif
                 </div>
                 <div class="mt-1">
                     <a href="{{ $character->url }}" class="h5 mb-0">@if(!$character->is_visible) <i class="fas fa-eye-slash"></i> @endif {{ $character->fullName }}</a>
