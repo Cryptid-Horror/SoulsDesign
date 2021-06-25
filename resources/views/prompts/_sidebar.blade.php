e76313f27bf72aeb1bd03b2abaccef75fcbdd8f6
Cryptid-Horror <matterintothevoid@gmail.com>
2021-06-25 10:01:59 -0500

    Updated prompt side bar to include new categories. Makes a cleaner search function for the missing things like nesting submissions/etc.

--- resources/views/prompts/_sidebar.blade.php
+++ resources/views/prompts/_sidebar.blade.php
@@ -15,18 +15,25 @@
     </li>
   <li class="sidebar-header"><a =class="card-link"><a href="{{ url('prompts') }}">Activites and Quests</a></a></li>
      <li class="sidebar-section">
-        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Daily%20Activities') }}" class="{{ set_active('prompts/prompt-categories?name=Daily%20Activities') }}">Daily activities</a></div>
-        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Basic%20Quests') }}" class="{{ set_active('prompts/prompt-categories?name=Basic%20Quests') }}">Basic Quests</a></div>
-        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Intermediate%20Quests') }}" class="{{ set_active('prompts/prompt-categories?name=Intermediate%20Quests') }}">Intermediate Quests</a></div>
-        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Master%20Quests') }}" class="{{ set_active('prompts/prompt-categories?name=Master%20Quests') }}">Master Quests</a></div>
-        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Side%20Quests') }}" class="{{ set_active('prompts/prompt-categories?name=Side%20Quests') }}">Side Quests</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Adoption%20Center') }}" class="{{ set_active('prompts/prompt-categories?name=Adoption%20Center') }}">Adoption Center</a></div>
         <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Arena') }}" class="{{ set_active('prompts/prompt-categories?name=Arena') }}">Arena</a></div>
-        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Call%20to%20Arms') }}" class="{{ set_active('prompts/prompt-categories?name=Call%20to%20Arms') }}">Call To Arms</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Daily%20Activities') }}" class="{{ set_active('prompts/prompt-categories?name=Daily%20Activities') }}">Daily activities</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=The%20Grand%20Hunt') }}" class="{{ set_active('prompts/prompt-categories?name=The%20Grand%20Hunt') }}">Grand Hunt</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Nesting%20and%20Hatchery') }}" class="{{ set_active('prompts/prompt-categories?name=Nesting%20and%20Hatchery') }}">Hatchery</a></div>
         <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Healing%20Shrine') }}" class="{{ set_active('prompts/prompt-categories?name=Healing%20Shrine') }}">Healing Shrine</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Nesting%20and%20Hatchery') }}" class="{{ set_active('prompts/prompt-categories?name=Nesting%20and%20Hatchery') }}">Nesting submissions</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Rank%20Updates') }}" class="{{ set_active('prompts/prompt-categories?name=Rank%20Updates') }}">Primal Points</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Rank%20Updates') }}" class="{{ set_active('prompts/prompt-categories?name=Rank%20Updates') }}">Mastery Points</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Basic%20Quests') }}" class="{{ set_active('prompts/prompt-categories?name=Basic%20Quests') }}">Quests: Basic</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Intermediate%20Quests') }}" class="{{ set_active('prompts/prompt-categories?name=Intermediate%20Quests') }}">Quests: Intermediate</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Master%20Quests') }}" class="{{ set_active('prompts/prompt-categories?name=Master%20Quests') }}">Quests: Master</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Side%20Quests') }}" class="{{ set_active('prompts/prompt-categories?name=Side%20Quests') }}">Quests: Sides</a></div>
+
     </li>
   <li class="sidebar-header"><a =class="card-link"><a href="{{ url('prompts') }}">Events</a></a></li>
      <li class="sidebar-section">
-        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Story%20Events') }}" class="{{ set_active('prompts/prompt-categories?name=Story%20Events') }}">Story Events</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=The%20Vortex') }}" class="{{ set_active('prompts/prompt-categories?name=The%20Vortex') }}">The Vortex</a></div>
+        <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Story%20Events') }}" class="{{ set_active('prompts/prompt-categories?name=Story%20Events') }}">Live Story Event</a></div>
         <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=General%20Events') }}" class="{{ set_active('prompts/prompt-categories?name=General%20Events') }}">General Events</a></div>
         <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Annual%20Events') }}" class="{{ set_active('prompts/prompt-categories?name=Annual%20Events') }}">Annual Events</a></div>
         <div class="sidebar-item"><a href="{{ url('prompts/prompt-categories?name=Misc%20Events') }}" class="{{ set_active('prompts/prompt-categories?name=Misc%20Events') }}">Miscellanious Events</a></div>
