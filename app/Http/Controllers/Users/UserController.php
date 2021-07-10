23c4d1b62f80b28df70a59a4bb89b9e03e8b2040
Cryptid-Horror <matterintothevoid@gmail.com>
2021-07-10 02:15:54 -0500

    I just wanna sleep

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -254,7 +254,6 @@ class UserController extends Controller
      * @param  string  $name
      * @return \Illuminate\Contracts\Support\Renderable
      */
-<
     public function getUserAwardCase($name)
     {
         $categories = AwardCategory::orderBy('sort', 'DESC')->get();
