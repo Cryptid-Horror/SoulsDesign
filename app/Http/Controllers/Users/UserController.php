3afb1d8c532066dcd1d83e62d31319dd6ae8b505
Cryptid-Horror <matterintothevoid@gmail.com>
2021-07-10 02:18:56 -0500

    LET ME DIE

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -101,7 +101,6 @@ class UserController extends Controller
             'awards' => $this->user->awards()->orderBy('user_awards.updated_at', 'DESC')->take(4)->get(),
             'sublists' => Sublist::orderBy('sort', 'DESC')->get(),
             'characters' => $characters,
-            'armours' => $armours,
             'gears' => $this->user->gears()->orderBy('user_gears.updated_at', 'DESC')->take(4)->get(),
             'weapons' => $this->user->weapons()->orderBy('user_weapons.updated_at', 'DESC')->take(4)->get(),
             'armours' => $gears->union($weapons),
