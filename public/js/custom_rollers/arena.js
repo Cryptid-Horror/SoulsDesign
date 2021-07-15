6ac8b2c56b634ee5e1b317ca76100143dfe4cd2f
Cryptid-Horror <matterintothevoid@gmail.com>
2021-07-15 00:36:05 -0500

    Maybe it needs to just be activity

--- public/js/custom_rollers/arena.js
+++ public/js/custom_rollers/arena.js
@@ -60,7 +60,7 @@ function rollBchallenger(){
 	
 	for (var m = 0; m < lootSize; m++) {
 		 itemlist += "<br>";
-		if (activity.value == "base_1"){rollBchallenger();}
+		if (activity == "base_1"){rollBchallenger();}
 		else if (activity.value == "basewarrior"){rollBwarrior();}
 		else if (activity.value == "basegladiator"){rollBgladiator();}
         else if (activity.value == "basechampion"){rollBchampion();}
