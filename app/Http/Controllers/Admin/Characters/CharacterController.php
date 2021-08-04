d8bdeffbed1c51b170a3fc97b47ac168fcaaf66f
Draginraptor <draginraptor@gmail.com>
2021-08-05 01:50:27 +0800

    fix: invalid index 'thumbnail' when trying to create MYO with a custom image

--- app/Http/Controllers/Admin/Characters/CharacterController.php
+++ app/Http/Controllers/Admin/Characters/CharacterController.php
@@ -177,7 +177,7 @@ class CharacterController extends Controller
         $data = $request->only([
             'user_id', 'owner_url', 'name',
             'description', 'is_visible', 'is_giftable', 'is_tradeable', 'is_sellable',
-            'sale_value', 'transferrable_at', 'use_custom_thumbnail',
+            'sale_value', 'transferrable_at', 'use_cropper',
             'x0', 'x1', 'y0', 'y1',
             'designer_id', 'designer_url',
             'artist_id', 'artist_url',
