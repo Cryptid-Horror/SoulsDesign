d62b45add22d3e13c112d8c77b34d13931efc55d
Cryptid-Horror <matterintothevoid@gmail.com>
2021-08-13 19:27:52 -0500

    Attempts to set up authetication for discord and twitter

--- config/lorekeeper/sites.php
+++ config/lorekeeper/sites.php
@@ -43,8 +43,8 @@ return [
         'regex' => '/twitter\.com\/([A-Za-z0-9_-]+)/',
         'link' => 'twitter.com',
         'icon' => 'fab fa-twitter',
-        'auth' => 0,
-        'primary_alias' => 0
+        'auth' => 1,
+        'primary_alias' => 1
     ],
 
     'instagram' => [
@@ -92,8 +92,8 @@ return [
         'display_name' => 'discord',
         'regex' => '/([A-Za-z0-9_-]+)#([0-9]+)/m',
         'icon' => 'fab fa-discord',
-        'auth' => 0,
-        'primary_alias' => 0
+        'auth' => 1,
+        'primary_alias' => 1
     ],
 
     /**********************************************************************************************
