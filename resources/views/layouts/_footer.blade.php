309cdb2e4ae5ddfe95f9e92c697d0eb478aa2b8b
Cryptid-Horror <matterintothevoid@gmail.com>
2021-09-07 13:39:33 -0500

    I'm very bad at this. Think it is the ul class making the little icons not line up right.

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-light">
-    <ul class="navbar-nav ml-auto mr-auto">
+    <ul>
         <li class="nav-item"><a href="mailto:soulsbetweenstaff@gmail.com" class="nav-link"><i class="fas fa-envelope-square"></i></a></li>
         <li class="nav-item"><a href="https://discord.gg/ZqtG7jf" class="nav-link"><i class="fab fa-discord"></i></a></li>
         <li class="nav-item"><a href="http://deviantart.com/souls-between" class="nav-link"><i class="fab fa-deviantart"></i></a></li>
