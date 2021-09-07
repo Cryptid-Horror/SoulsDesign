79dd0d3ffbfa2ef6468e77ab5d87196b4d48e38a
Cryptid-Horror <matterintothevoid@gmail.com>
2021-09-07 13:37:42 -0500

    Original formatting didn't work, retry.

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,12 +4,15 @@
         <li class="nav-item"><a href="https://discord.gg/ZqtG7jf" class="nav-link"><i class="fab fa-discord"></i></a></li>
         <li class="nav-item"><a href="http://deviantart.com/souls-between" class="nav-link"><i class="fab fa-deviantart"></i></a></li>
         <br>
-        <li class="nav-item"><a href="{{ url('info/about') }}" class="nav-link">About</a></li> | 
-        <li class="nav-item"><a href="{{ url('info/terms') }}" class="nav-link">Terms</a></li> | 
-        <li class="nav-item"><a href="{{ url('info/privacy') }}" class="nav-link">Privacy</a></li> | 
-        <li class="nav-item"><a href="{{ url('info/Player_Guide') }}" class="nav-link">Player Guide</a></li> | 
-        <li class="nav-item"><a href="https://www.soulsbetween.com/info/_StaffApps"class="nav-link">Join the Team</a></li> | 
-        <li class="nav-item"><a href="https://github.com/corowne/lorekeeper" class="nav-link">Lorekeeper</a></li> | 
+</ul>
+        <ul class="navbar-nav ml-auto mr-auto">
+
+        <li class="nav-item"><a href="{{ url('info/about') }}" class="nav-link">About</a></li> 
+        <li class="nav-item"><a href="{{ url('info/terms') }}" class="nav-link">Terms</a></li> 
+        <li class="nav-item"><a href="{{ url('info/privacy') }}" class="nav-link">Privacy</a></li> 
+        <li class="nav-item"><a href="{{ url('info/Player_Guide') }}" class="nav-link">Player Guide</a></li> 
+        <li class="nav-item"><a href="https://www.soulsbetween.com/info/_StaffApps"class="nav-link">Join the Team</a></li> 
+        <li class="nav-item"><a href="https://github.com/corowne/lorekeeper" class="nav-link">Lorekeeper</a></li> 
         <li class="nav-item"><a href="{{ url('credits') }}" class="nav-link">Credits</a></li>
     </ul>
 </nav>
