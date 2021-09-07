dc3a3054a94550a8e38015da1413b0c5688e546c
Cryptid-Horror <matterintothevoid@gmail.com>
2021-09-07 13:36:45 -0500

    Footer changes.

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,14 +1,15 @@
 <nav class="navbar navbar-expand-md navbar-light">
     <ul class="navbar-nav ml-auto mr-auto">
-        <li class="nav-item"><a href="{{ url('info/about') }}" class="nav-link">About</a></li>
-        <li class="nav-item"><a href="{{ url('info/Player_Guide') }}" class="nav-link">Player Guide</a></li>
-        <li class="nav-item"><a href="{{ url('info/terms') }}" class="nav-link">Terms</a></li>
-        <li class="nav-item"><a href="{{ url('info/privacy') }}" class="nav-link">Privacy</a></li>
-        <li class="nav-item"><a href="mailto:soulsbetweenstaff@gmail.com" class="nav-link">Contact</a></li>
-        <li class="nav-item"><a href="https://www.soulsbetween.com/info/_StaffApps"class="nav-link">Join the Team</a></li>
-        <li class="nav-item"><a href="https://discord.gg/ZqtG7jf" class="nav-link">Discord</a></li>
-        <li class="nav-item"><a href="http://deviantart.com/souls-between" class="nav-link">deviantART</a></li>
-        <li class="nav-item"><a href="https://github.com/corowne/lorekeeper" class="nav-link">Lorekeeper</a></li>
+        <li class="nav-item"><a href="mailto:soulsbetweenstaff@gmail.com" class="nav-link"><i class="fas fa-envelope-square"></i></a></li>
+        <li class="nav-item"><a href="https://discord.gg/ZqtG7jf" class="nav-link"><i class="fab fa-discord"></i></a></li>
+        <li class="nav-item"><a href="http://deviantart.com/souls-between" class="nav-link"><i class="fab fa-deviantart"></i></a></li>
+        <br>
+        <li class="nav-item"><a href="{{ url('info/about') }}" class="nav-link">About</a></li> | 
+        <li class="nav-item"><a href="{{ url('info/terms') }}" class="nav-link">Terms</a></li> | 
+        <li class="nav-item"><a href="{{ url('info/privacy') }}" class="nav-link">Privacy</a></li> | 
+        <li class="nav-item"><a href="{{ url('info/Player_Guide') }}" class="nav-link">Player Guide</a></li> | 
+        <li class="nav-item"><a href="https://www.soulsbetween.com/info/_StaffApps"class="nav-link">Join the Team</a></li> | 
+        <li class="nav-item"><a href="https://github.com/corowne/lorekeeper" class="nav-link">Lorekeeper</a></li> | 
         <li class="nav-item"><a href="{{ url('credits') }}" class="nav-link">Credits</a></li>
     </ul>
 </nav>
