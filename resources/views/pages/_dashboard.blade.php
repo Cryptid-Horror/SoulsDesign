682a0ffd03a848c672f22d16a536397da0aeeda6
Cryptid-Horror <matterintothevoid@gmail.com>
2021-09-07 14:13:16 -0500

    More playing with the same thing as before.

--- resources/views/pages/_dashboard.blade.php
+++ resources/views/pages/_dashboard.blade.php
@@ -63,7 +63,7 @@
 </div>
 
 <div class="card bg-dark text-white">
-  <img class="card-img" src="..." alt="Card image">
+  <img class="card-img" src="{{ asset('images/awards.png') }}" alt="Card image">
   <div class="card-img-overlay">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
