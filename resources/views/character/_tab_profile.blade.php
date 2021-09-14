c6a7d66c48a6c666b15100aa18651030d2c794a2
Cryptid-Horror <matterintothevoid@gmail.com>
2021-09-14 02:54:54 -0500

    Trying to move the slot count...

--- resources/views/character/_tab_profile.blade.php
+++ resources/views/character/_tab_profile.blade.php
@@ -32,6 +32,30 @@
     <img style="height:150px;" src="{{ $character->rankImageUrl }}"><br>
     <br>
     <b>Ouroboros Emblem:</b> {{ $character->ouroboros ? 'Complete' : 'Incomplete' }}<br>
+    @if($character->ouroboros)
+        <b>Slots:</b> 
+        @if($character->rank != 'Primordial')
+            {{ $character->slots_used }} /
+            @switch($character->rank)
+                @case('Fledgling')
+                    5
+                    @break
+                @case('Primal')
+                    15
+                    @break
+                @case('Ancient')
+                    30
+                    @break
+                @default
+                    Undefined
+                    @break
+            @endswitch
+            Used
+        @else
+            ∞
+        @endif
+    @endif
+    <br>
     <b>Taming:</b> {{ $character->taming ? $character->taming.' Taming Complete' : 'Incomplete' }}<br>
     <ul>
         @switch($character->taming)
@@ -130,29 +154,7 @@
     <b>Personality:</b><br>
     {!! $character->profile->parsed_text ?? 'N/A' !!}
     <br><br>
-    @if($character->ouroboros)
-        <b>Slots:</b> 
-        @if($character->rank != 'Primordial')
-            {{ $character->slots_used }} /
-            @switch($character->rank)
-                @case('Fledgling')
-                    5
-                    @break
-                @case('Primal')
-                    15
-                    @break
-                @case('Ancient')
-                    30
-                    @break
-                @default
-                    Undefined
-                    @break
-            @endswitch
-            Used
-        @else
-            ∞
-        @endif
-    @endif
+   
     <br>
     <b>Generation:</b> {!! $character->rarity->displayName ?? '-' !!}<br>
     <b>Lineage:</b><br>
