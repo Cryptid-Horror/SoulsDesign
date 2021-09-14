69b05a9df67e97270490f3a74e18321cdef45f67
Cryptid-Horror <matterintothevoid@gmail.com>
2021-09-14 05:14:24 -0500

    Trying to at least see faction.

--- resources/views/character/_tab_profile.blade.php
+++ resources/views/character/_tab_profile.blade.php
@@ -150,33 +150,10 @@
     <b>Nicknames:</b> {{ $character->nicknames ? $character->nicknames : 'N/A' }}<br>
     <b>Gender/Pronouns:</b> {{ $character->gender_pronouns ? $character->gender_pronouns : '-' }}<br>
     @if($character->homeSetting)<b>Location:</b> {{ $character->homeSetting }}@endif <br>
-    @if($image->character->homeSetting)
-                <div class="row">
-
-                    <div class="col-lg-4 col-md-6 col-4"><h5>Home</h5></div>
-                    <div class="col-lg-8 col-md-6 col-8">{!! $image->character->location ? $image->character->location : 'None' !!}</div>
-
-                    <div class="col-lg-4 col-md-6 col-4"><h5>Class</h5></div>
-                    <div class="col-lg-8 col-md-6 col-8">{!! $image->character->class_id ? $image->character->class->displayName : 'None' !!} 
-                        @if(Auth::check())
-                        @if(Auth::user()->isStaff || Auth::user()->id == $image->character->user_id && $image->character->class_id == null) 
-                        <a href="#" class="btn btn-outline-info btn-sm edit-class ml-1" data-id="{{ $image->character->id }}"><i class="fas fa-cog"></i></a>
-                        @endif
-                        @endif
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 col-4"><h5>Species</h5></div>
-                    <div class="col-lg-8 col-md-6 col-8">{!! $image->species_id ? $image->species->displayName : 'None' !!}</div>
-
-                </div>
-                @if($image->character->factionSetting)
-                    <div class="row">
-                        <div class="col-lg-4 col-md-6 col-4"><h5>Faction</h5></div>
-                        <div class="col-lg-8 col-md-6 col-8">{!! $image->character->faction ? $image->character->currentFaction : 'None' !!}{!! $character->factionRank ? ' ('.$character->factionRank->name.')' : null !!}</div>
-                    </div>
-                @endif
-            @endif
+    @if($image->character->factionSetting)
+    <b>Faction:</b> {!! $image->character->faction ? $image->character->currentFaction : 'None' !!}{!! $character->factionRank ? ' ('.$character->factionRank->name.')' : null !!}
+    @endif
+    <br>
     <b>Personality:</b><br>
     {!! $character->profile->parsed_text ?? 'N/A' !!}
     <br><br>
