48d57486f0668ed4bf108af5f387e5f338201f92
Draginraptor <draginraptor@gmail.com>
2021-09-14 14:08:41 +0800

    feat: hovereffect header moved to be over the images; added margin bottom on the widget itself

--- resources/views/widgets/_hovereffect_image.blade.php
+++ resources/views/widgets/_hovereffect_image.blade.php
@@ -1,11 +1,13 @@
-<div class="hovereffect mw-100 d-flex justify-content-center">
-    <img class="img-responsive" src="{{ $imageUrl }}" alt="">
-    <div class="overlay">
-        <h2>{{ $header }}</h2>
-        <ul class="list-group list-group-flush">
-            @foreach($links as $title => $link)
-            <li class="list-group-item"><a href="{{ $link }}">{{ $title }}</a></li>
-            @endforeach
-        </ul>
+<div class="mb-5">
+    <h2 class="text-center mb-0">{{ $header }}</h2>
+    <div class="hovereffect mw-100 d-flex justify-content-center">
+        <img class="img-responsive" src="{{ $imageUrl }}" alt="">
+        <div class="overlay">
+            <ul class="list-group list-group-flush">
+                @foreach($links as $title => $link)
+                <li class="list-group-item"><a href="{{ $link }}">{{ $title }}</a></li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 </div>
