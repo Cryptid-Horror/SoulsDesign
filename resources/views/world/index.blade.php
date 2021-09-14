8f0c031e80578ccd94ddba29c8b46becb1835df9
Cryptid-Horror <matterintothevoid@gmail.com>
2021-09-14 01:34:16 -0500

    Index updated to have hover widget.

--- resources/views/world/index.blade.php
+++ resources/views/world/index.blade.php
@@ -6,6 +6,64 @@
 {!! breadcrumbs(['Encyclopedia' => 'world']) !!}
 
 <h1>World</h1>
+
+
+
+<div class="row">
+        <div class="col-xl-3 col-sm-6 d-flex justify-content-center">
+            @include('widgets._hovereffect_image', [
+                'imageUrl' => asset('images/xdd.png'),
+                'header' => 'Species',
+                'links' => [
+                    'Species' => url('world/species'),
+                    'Subtypes' => url('world/subtypes'),
+                    'Character Categories' => url('world/character-categories')
+                ]
+            ])
+        </div>
+        <div class="col-xl-3 col-sm-6 d-flex justify-content-center">
+            @include('widgets._hovereffect_image', [
+                'imageUrl' => asset('images/characters2.png'),
+                'header' => 'Traits',
+                'links' => [
+                    'Categories' => url('world/trait-categories'),
+                    'All Traits' => url('world/traits'),
+                    'Rarities' => url('world/rarities')
+                ]
+            ])
+        </div>
+        <div class="col-xl-3 col-sm-6 d-flex justify-content-center">
+            @include('widgets._hovereffect_image', [
+                'imageUrl' => asset('images/honeyedit.png'),
+                'header' => 'Combat Stats',
+                'links' => [
+                    'Combat Classes' => url('world/character-classes'),
+                    'Gear Categories' => url('world/gear-categories'),
+                    'All Gear' => url('world/gear'),
+                    'Pet Categories' => url('world/pet-categories'),
+                    'All Pets' => url)('world/pets'),
+                ]
+            ])
+        </div>
+        <div class="col-xl-3 col-sm-6 d-flex justify-content-center">
+            @include('widgets._hovereffect_image', [
+                'imageUrl' => asset('images/flower2.png'),
+                'header' => 'Items, Etc',
+                'links' => [
+                    'Item Categories' => url('world/item-categories'),
+                    'All Items' => url('world/items'),
+                    'All Recipes' => url('world/recipes'),
+                    'Currencies' => url('world/currencies')
+                    'Categories' => url('world/award-categories'),
+                    'All Awards' => url('world/awards'),
+                ]
+            ])
+        </div>
+    </div>
+
+
+
+{{--
 <div class="row">
     <div class="col-md-6">
         <div class="card mb-4">
@@ -47,4 +105,8 @@
         </div>
     </div>
 </div>
+--}}
+
+
+
 @endsection
