97d08f9ba237b4b64588bf70376f7d13fe0eccbd
Cryptid-Horror <matterintothevoid@gmail.com>
2021-10-10 19:55:33 -0500

    no message

--- resources/views/design_guides/index.blade.php
+++ resources/views/design_guides/index.blade.php
@@ -123,7 +123,7 @@
 								@foreach($free_chunk as $free)
 									<div class="col-md-6 col-lg-3 mb-2">
 										<img src="{{ asset('images/design_guides/Free_'.str_replace(' ', '_', $free[0]).'.png') }}" width="90%" style="max-width:200px;">
-										<a class="btn btn-info" href="{{ url('design/freee/'.strtolower(str_replace(' ', '_', $free[0]))) }}">{{ $free[0].' ('.$free[1].')' }}</a>
+										<a class="btn btn-info" href="{{ url('design/free/'.strtolower(str_replace(' ', '_', $free[0]))) }}">{{ $free[0].' ('.$free[1].')' }}</a>
 									</div>
 								@endforeach
 							</div>
