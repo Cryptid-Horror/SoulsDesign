1954326352d748fc62aee1c27bd98efece0e4d1d
Cryptid-Horror <matterintothevoid@gmail.com>
2021-10-23 16:11:48 -0500

    Reduced AT to 40 from 60

--- public/js/custom_rollers/Developer_NestingRoller_v4.0.js
+++ public/js/custom_rollers/Developer_NestingRoller_v4.0.js
@@ -1464,7 +1464,7 @@ function generateTrait(traitToGen) {
 	if (document.getElementById("AT").checked) {
 		if (!destroyedModifiers.includes("Aether Tonic destroyed.<br>"))
 			destroyedModifiers += "Aether Tonic destroyed.<br>";
-		bonus = 60;
+		bonus = 40;
 	}
 	
 	var roll = randRange(100);
