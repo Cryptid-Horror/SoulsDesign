b621e2966bd1aa347b902a52d09e7bdb07ba7262
Cryptid-Horror <matterintothevoid@gmail.com>
2021-12-17 12:54:14 -0600

    Renamed link

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -258,7 +258,7 @@
                             </a>
 
                             <a class="dropdown-item" href="{{ Auth::user()->url.'/breeding-permissions' }}">
-                                View genotypes
+                                View Nesting Permissions
                             </a>
                             
                             <a class="dropdown-item" href="{{ url('inventory') }}">
