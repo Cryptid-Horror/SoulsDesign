616f66f79bdf3cf524609dba1f6fdbea41c5df0e
Cryptid-Horror <matterintothevoid@gmail.com>
2022-01-27 16:02:42 -0600

    Some more playing around for making hoards have tabs.

--- resources/views/user/inventory.blade.php
+++ resources/views/user/inventory.blade.php
@@ -9,43 +9,36 @@
     Inventory
 </h1>
 
-<div class="card character-bio">
+    @foreach($items as $categoryId=>$categoryItems)
+    <div class="card mb-3 inventory-category">
     <div class="card-header">
         <ul class="nav nav-tabs card-header-tabs">
-            @foreach($items as $categoryId=>$categoryItems)
+        @foreach($items as $categoryId=>$categoryItems)
                 <li class="nav-item">
                     <a class="nav-link {{ $loop->first ? 'active' : '' }}" id="categoryTab-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}" data-toggle="tab" href="#category-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}" role="tab">
-                        {!! isset($categories[$categoryId]) ? $categories[$categoryId]->name : 'Miscellaneous' !!}
+                        {!! isset($categories[$categoryId]) ? '<a href="'.$categories[$categoryId]->searchUrl.'">'.$categories[$categoryId]->name.'</a>' : 'Miscellaneous' !!}
                     </a>
                 </li>
             @endforeach
         </ul>
-    </div>
-    <div class="card-body tab-content">
-        @foreach($items as $categoryId=>$categoryItems)
-            <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="category-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}">
-                @foreach($categoryItems->chunk(4) as $chunk)
-                    <div class="row mb-3">
-                        @foreach($chunk as $item)
-                            <div class="col-sm-3 col-6 text-center inventory-item" data-id="{{ $item->pivot->id }}">
-                                <div class="mb-1">
-                                    <a href="#" class="inventory-stack"><img src="{{ $item->imageUrl }}" alt="{{ $item->name }}" /></a>
-                                </div>
-                                <div>
-                                    <a href="#" class="inventory-stack inventory-stack-name"><strong>{{ $item->name }}</strong></a>
-                                    <div><strong>Cost: </strong> {!! $currencies[$item->pivot->currency_id]->display((int)$item->pivot->cost) !!}</div>
-                                    @if($item->pivot->is_limited_stock) <div>Stock: {{ $item->pivot->quantity }}</div> @endif
-                                    @if($item->pivot->purchase_limit) <div class="text-danger">Max {{ $item->pivot->purchase_limit }} per user</div> @endif
-                                </div>
+        <div class="card-body inventory-body collapse show" id="{!! clean(isset($categories[$categoryId]) ? str_replace(' ', '', $categories[$categoryId]->name) : 'miscellaneous') !!}">
+            @foreach($categoryItems->chunk(4) as $chunk)
+                <div class="row mb-3">
+                    @foreach($chunk as $itemId=>$stack)
+                        <div class="col-sm-3 col-6 text-center inventory-item" data-id="{{ $stack->first()->pivot->id }}" data-name="{{ $user->name }}'s {{ $stack->first()->name }}">
+                            <div class="mb-1">
+                                <a href="#" class="inventory-stack"><img src="{{ $stack->first()->imageUrl }}" alt="{{ $stack->first()->name }}" /></a>
                             </div>
-                        @endforeach
-                    </div>
-                @endforeach
-            </div>
-        @endforeach
+                            <div>
+                                <a href="#" class="inventory-stack inventory-stack-name">{{ $stack->first()->name }} x{{ $stack->sum('pivot.count') }}</a>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @endforeach
+        </div>
     </div>
-</div>
-
+@endforeach
 
 
 
