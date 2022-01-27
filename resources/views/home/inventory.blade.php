6799a54c02454f8d4a45f9a4f32263e215dd0e42
Cryptid-Horror <matterintothevoid@gmail.com>
2022-01-27 16:14:57 -0600

    Attempt two...

--- resources/views/home/inventory.blade.php
+++ resources/views/home/inventory.blade.php
@@ -14,7 +14,46 @@
 
 <p>This is your inventory. Click on an item to view more details and actions you can perform on it.</p>
 
-@foreach($items as $categoryId=>$categoryItems)
+
+<div class="card character-bio">
+    <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs">
+            @foreach($items as $categoryId=>$categoryItems)
+                <li class="nav-item">
+                    <a class="nav-link {{ $loop->first ? 'active' : '' }}" id="categoryTab-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}" data-toggle="tab" href="#category-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}" role="tab">
+                        {!! isset($categories[$categoryId]) ? $categories[$categoryId]->name : 'Miscellaneous' !!}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="card-body tab-content">
+        @foreach($items as $categoryId=>$categoryItems)
+            <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="category-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}">
+            @foreach($categoryItems->chunk(4) as $chunk)
+                <div class="row mb-3">
+                    @foreach($chunk as $itemId=>$stack)
+                        <div class="col-sm-3 col-6 text-center inventory-item" data-id="{{ $stack->first()->pivot->id }}" data-name="{{ $user->name }}'s {{ $stack->first()->name }}">
+                            @if($stack->first()->has_image)
+                                <div class="mb-1">
+                                    <a href="#" class="inventory-stack"><img src="{{ $stack->first()->imageUrl }}" alt="{{ $stack->first()->name }}"/></a>
+                                </div>
+                            @endif
+                            <div>
+                                <a href="#" class="inventory-stack inventory-stack-name">{{ $stack->first()->name }} x{{ $stack->sum('pivot.count') }}</a>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endforeach
+    </div>
+</div>
+
+
+{-- @foreach($items as $categoryId=>$categoryItems)
     <div class="card mb-3 inventory-category">
         <h5 class="card-header inventory-header">
             {!! isset($categories[$categoryId]) ? '<a href="'.$categories[$categoryId]->searchUrl.'">'.$categories[$categoryId]->name.'</a>' : 'Miscellaneous' !!}
@@ -39,7 +78,7 @@
             @endforeach
         </div>
     </div>
-@endforeach
+@endforeach --}
 <div class="text-right mb-4">
     <a href="{{ url(Auth::user()->url.'/item-logs') }}">View logs...</a>
 </div>
