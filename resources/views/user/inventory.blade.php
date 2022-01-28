a194fa97677ca0cd9aff6ae704158dd88b7306a5
Cryptid-Horror <matterintothevoid@gmail.com>
2022-01-27 20:24:12 -0600

    Added changes to inventory

--- resources/views/user/inventory.blade.php
+++ resources/views/user/inventory.blade.php
@@ -4,37 +4,56 @@
 
 @section('profile-content')
 {!! breadcrumbs(['Users' => 'users', $user->name => $user->url, 'Inventory' => $user->url . '/inventory']) !!}
+@section('profile-title') {{ $user->name }}'s Profile @endsection
 
+@section('meta-img') {{ asset('/images/avatars/'.$user->avatar) }} @endsection
 <h1>
-    Inventory
-</h1>
+    <img src="/images/avatars/{{ $user->avatar }}" style="width:125px; height:125px; float:left; border-radius:50%; margin-right:25px;" alt="{{ $user->name }}" ></h1>
 
-    @foreach($items as $categoryId=>$categoryItems)
-    <div class="card mb-3 inventory-category">
-        <h5 class="card-header inventory-header">
-            {!! isset($categories[$categoryId]) ? '<a href="'.$categories[$categoryId]->searchUrl.'">'.$categories[$categoryId]->name.'</a>' : 'Miscellaneous' !!}
-            <a class="small inventory-collapse-toggle collapse-toggle collapsed" href="#{!! clean(isset($categories[$categoryId]) ? str_replace(' ', '', $categories[$categoryId]->name) : 'miscellaneous') !!}" data-toggle="collapse">Show</a></h3>
-        </h5>
-        <div class="card-body inventory-body collapse show" id="{!! clean(isset($categories[$categoryId]) ? str_replace(' ', '', $categories[$categoryId]->name) : 'miscellaneous') !!}">
+<h1>{!! Auth::user()->displayName !!}'s Hoard</h1>
+<h1>
+    <div class="float-left mb-3">
+        <a class="btn btn-primary" href="{{ url('inventory/account-search') }}"><i class="fas fa-search"></i> Account Search</a>
+    </div>
+</h1>
+<br><br><br>
+<p>This is your inventory. Click on an item to view more details and actions you can perform on it.</p>
+
+<div class="card character-bio">
+    <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs">
+            @foreach($items as $categoryId=>$categoryItems)
+                <li class="nav-item">
+                    <a class="nav-link {{ $loop->first ? 'active' : '' }}" id="categoryTab-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}" data-toggle="tab" href="#category-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}" role="tab">
+                        {!! isset($categories[$categoryId]) ? $categories[$categoryId]->name : 'Miscellaneous' !!}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="card-body tab-content">
+        @foreach($items as $categoryId=>$categoryItems)
+            <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="category-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}">
             @foreach($categoryItems->chunk(4) as $chunk)
                 <div class="row mb-3">
                     @foreach($chunk as $itemId=>$stack)
                         <div class="col-sm-3 col-6 text-center inventory-item" data-id="{{ $stack->first()->pivot->id }}" data-name="{{ $user->name }}'s {{ $stack->first()->name }}">
-                            <div class="mb-1">
-                                <a href="#" class="inventory-stack"><img src="{{ $stack->first()->imageUrl }}" alt="{{ $stack->first()->name }}" /></a>
-                            </div>
+                            @if($stack->first()->has_image)
+                                <div class="mb-1">
+                                    <a href="#" class="inventory-stack"><img src="{{ $stack->first()->imageUrl }}" alt="{{ $stack->first()->name }}"/></a>
+                                </div>
+                            @endif
                             <div>
                                 <a href="#" class="inventory-stack inventory-stack-name">{{ $stack->first()->name }} x{{ $stack->sum('pivot.count') }}</a>
                             </div>
-                        </div>
-                    @endforeach
-                </div>
-            @endforeach
-        </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
     </div>
-@endforeach
-
-
+</div>
 
 <h3>Latest Activity</h3>
 <div class="row ml-md-2 mb-4">
