4fb72cdbf43c8223d13373f4ee7e17f6e21b8123
Cryptid-Horror <matterintothevoid@gmail.com>
2022-01-27 20:10:52 -0600

    Yeah didn't work :C

--- resources/views/pages/_dashboard.blade.php
+++ resources/views/pages/_dashboard.blade.php
@@ -1,11 +1,5 @@
 
 
-@section('profile-title') {{ $user->name }}'s Profile @endsection
-
-@section('meta-img') {{ asset('/images/avatars/'.$user->avatar) }} @endsection
-<h1>
-    <img src="/images/avatars/{{ $user->avatar }}" style="width:125px; height:125px; float:left; border-radius:50%; margin-right:25px;" alt="{{ $user->name }}" ></h1>
-
 <h1>Welcome, {!! Auth::user()->displayName !!}!</h1>
 <div class="card mb-4 timestamp">
     <div class="card-body">
