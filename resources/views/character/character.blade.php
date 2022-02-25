2e838f2564fff3e9f0503f4984063ed076a248c7
Draginraptor <draginraptor@gmail.com>
2022-02-25 14:27:28 +0800

    fix: when there is no current health

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -39,18 +39,22 @@
         <h4><span class="badge badge-dark float-center text-white mx-1" style="height: 20px; width: 15%;" data-toggle="tooltip" title="Current Character level.">Current Lvl: {{ $character->level->current_level }}</span></h4>
         <div class="m-4"><strong>Current Celestial Experience:</strong> <br>{{ $character->level->current_exp }} </div>
        
-    @php
-        $stats = $character->stats;
-        $health =$stats->shift();
-    @endphp
-    
-       <center><b><h4>Character Health</h4></b>
-    <div class="progress" style="height: 20px; width: 50%;" >
-        <div class="progress-bar bg-success text-dark h4" role="progressbar" aria-valuenow="{{ $health->current_count}}" aria-valuemin="0" aria-valuemax="{{ $health->current_count }}" style="height:100%; width:{{ isset($health->current_count) ? $health->current_count : 100 }}%">
-        {{ isset($health->current_count) ? round(($health->current_count/$health->count),3)*100 : 100 }}%
-        </div>
-    </div>
-    </center>
+        @php
+            $stats = $character->stats;
+            $health = $stats->shift();
+        @endphp
+        <center>
+            @if($health)
+                <b><h4>Character Health</h4></b>
+                <div class="progress" style="height: 20px; width: 50%;" >
+                    <div class="progress-bar bg-success text-dark h4" role="progressbar" aria-valuenow="{{ $health->current_count}}" aria-valuemin="0" aria-valuemax="{{ $health->current_count }}" style="height:100%; width:{{ isset($health->current_count) ? $health->current_count : 100 }}%">
+                    {{ isset($health->current_count) ? round(($health->current_count/$health->count),3)*100 : 100 }}%
+                    </div>
+                </div>
+            @else
+                Health data not yet initialized! Please visit the stats tab to initialize.
+            @endif
+        </center>
        <br><br>
         </div>
     </div>
