56247b799fef7155162780d58c3a2379d19336d8
Cryptid-Horror <matterintothevoid@gmail.com>
2022-02-24 21:27:47 -0600

    Attempt to add level bar to main profile on dragon because I am chaos

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -33,6 +33,47 @@
         <div class="text-right">You are viewing the full-size image. <a href="{{ $character->image->imageUrl }}">View watermarked image</a>?</div>
     @endif
 </div>
+
+
+<h1>
+   Celestial Rank Area
+</h1>
+
+<div class="container text-center">
+    <h1><span class="badge badge-dark float-center text-white mx-1" data-toggle="tooltip" title="Current Character level.">Current Lvl: {{ $character->level->current_level }}</span></h1>
+    
+    @if($next)
+    <p>Next Level: {{ $next->level}}</p>
+    {{ $character->level->current_exp}}/{{ $next->exp_required }}
+    <div class="progress">
+        <div class="progress-bar progress-bar-striped active" role="progressbar"
+        aria-valuenow="{{ $character->level->current_exp}}" aria-valuemin="0" aria-valuemax="{{ $next->exp_required }}" style="width:{{$width}}%">
+        {{ $character->level->current_exp}}/{{ $next->exp_required }}
+        </div>
+    </div>
+        @if($character->level->current_exp >= $next->exp_required)
+        <div class="text-center m-1">
+            <b><p>You have enough EXP to advance to the nex level!</p></b>
+        </div>
+        {!! Form::open(['url' => $character->url.'/level-area/up']) !!}
+
+            {!! Form::submit('Level up!', ['class' => 'btn btn-success mb-2']) !!}
+
+        {!! Form::close() !!}
+        @endif
+    @else
+        <p>You are at the max level!</p>
+    @endif
+
+    <div class="mb-4 mt-2 text-center">
+        <div class="card text-center">
+            <div class="m-4"><strong>Current Celestial Experience:</strong> <br>{{ $character->level->current_exp }} </div>
+            <div class="m-4"><strong>Current Available Stat Points:</strong> <br>{{ $character->level->current_points }}</div>
+        </div>
+    </div>
+</div>
+
+
 @php
         $stats = $character->stats;
         $health =$stats->shift();
