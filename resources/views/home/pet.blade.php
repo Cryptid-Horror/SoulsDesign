1671418fd2ca946b3f5d8d16daa8adb9ccf67355
Cryptid-Horror <matterintothevoid@gmail.com>
2022-02-27 15:58:19 -0600

    Added tab to home view of pets

--- resources/views/home/pet.blade.php
+++ resources/views/home/pet.blade.php
@@ -11,7 +11,47 @@
 
 <p>These are your pets. Click on a pet to view more details and actions you can perform on it.</p>
 
-@foreach($pets as $categoryId=>$categoryPets)
+
+<div class="card character-bio">
+    <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs">
+            @foreach($pets as $categoryId=>$categoryPets)
+                <li class="nav-item">
+                    <a class="nav-link {{ $loop->first ? 'active' : '' }}" id="categoryTab-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}" data-toggle="tab" href="#category-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}" role="tab">
+                        {!! isset($categories[$categoryId]) ? $categories[$categoryId]->name : 'Miscellaneous' !!}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="card-body tab-content">
+        @foreach($pets as $categoryId=>$categoryPets)
+            <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="category-{{ isset($categories[$categoryId]) ? $categoryId : 'misc'}}">
+            @foreach($categoryPets->chunk(4) as $chunk)
+                <div class="row mb-3">
+                    @foreach($chunk as $pet)
+                        <div class="col-sm-3 col-6 text-center inventory-item" data-id="{{ $pet->pivot->id }}" data-name="{{ $user->name }}'s {{ $pet->name }}">
+                            <div class="mb-1">
+                                <a href="#" class="inventory-stack"><img src="{{ $pet->VariantImage($pet->pivot->variant_id) }}" class="img-fluid"/></a>
+                            </div>
+                            <div>
+                                <a href="#" class="inventory-stack inventory-stack-name">{{ $pet->VariantName($pet->pivot->variant_id) }} {{ $pet->name }}</a>
+                            </div>
+                            <div>
+                                <span class="text-light badge badge-dark" style="font-size:95%;">{{ $pet->pivot->pet_name }}</span>
+                            </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
+    </div>
+</div>
+
+
+
+{{-- @foreach($pets as $categoryId=>$categoryPets)
     <div class="card mb-3 inventory-category">
         <h5 class="card-header inventory-header">
             {!! isset($categories[$categoryId]) ? '<a href="'.$categories[$categoryId]->searchUrl.'">'.$categories[$categoryId]->name.'</a>' : 'Miscellaneous' !!}
@@ -38,7 +78,7 @@
             @endforeach
         </div>
     </div>
-@endforeach
+@endforeach --}} 
 <div class="text-right mb-4">
     <a href="{{ url(Auth::user()->url.'/pet-logs') }}">View logs...</a>
 </div>
