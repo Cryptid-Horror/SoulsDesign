8ccf256d44437f630112802a9ee1595981e4a0ed
Cryptid-Horror <matterintothevoid@gmail.com>
2022-02-27 17:30:35 -0600

    Removed optional tag

--- resources/views/character/design/comments.blade.php
+++ resources/views/character/design/comments.blade.php
@@ -13,7 +13,7 @@
     <p>The comment section is NOT optional! Please refer to the design registration guide under the activity drop down for the forms necessary to submit your design! Not including this form will cause your design to be sent back! </p>
     {!! Form::open(['url' => 'designs/'.$request->id.'/comments']) !!}
         <div class="form-group">
-            {!! Form::label('Comments (Optional)') !!}
+            {!! Form::label('Comments') !!}
             {!! Form::textarea('comments', $request->comments, ['class' => 'form-control']) !!}
         </div>
         <div class="text-right">
