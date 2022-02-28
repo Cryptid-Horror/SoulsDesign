ab32f971699955b35a4b6e251734ff17212a78ae
Cryptid-Horror <matterintothevoid@gmail.com>
2022-02-27 20:44:14 -0600

    Listen I dont know what to do

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -131,7 +131,7 @@
                 @if(count($pets))
                     <div class="row">
                         @foreach($pets as $pet)
-                            <div class="col-md-3 col-6 profile-inventory-item">
+                            <div class="col-md-4 col-6 profile-inventory-item">
                                 <a href="#" class="inventory-stack"><img src="{{ $pet->variantimage($pet->pivot->variant_id) }}" class="img-fluid" style="width:100%;" data-toggle="tooltip" title="{{ $pet->name }}" alt="{{ $pet->name }}" />
                             </div>
                         @endforeach
