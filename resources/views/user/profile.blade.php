ffb7c7800932d386324b9010122f5db2d675493f
Draginraptor <draginraptor@gmail.com>
2022-04-23 00:44:39 +0800

    feat: improved profile format for mobile views

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -22,7 +22,9 @@
     @endif
     <span class="badge badge-info float-right text-white mx-1" data-toggle="tooltip" title="Current user level. Checkout the level area for more info.">Lvl: {{ $user->level->current_level }}</span>
 </h1>
-<div class="mb-1">
+{{-- For large screens --}}
+<div class="mb-1 d-none d-sm-block">
+    <div class="row">
         <div class="row col-md-6">
             <div class="col-md-4 col-4"><h5>Alias</h5></div>
             <div class="col-md-8 col-8">{!! $user->displayAlias !!}</div>
@@ -53,6 +55,41 @@
                 <div class="col-md-8 col-8">{!! $user->faction ? $user->faction->fullDisplayName : '-Deleted Faction-' !!}{!! $user->factionRank ? ' ('.$user->factionRank->name.')' : null !!}</div>
             </div>
         @endif
+    </div>
+</div>
+
+
+<div class="mb-1 d-xs-block d-sm-none">
+    <div class="row col-md-6">
+        <div class="col-md-4 col-4"><h5>Alias</h5></div>
+        <div class="col-md-8 col-8">{!! $user->displayAlias !!}</div>
+    </div>
+    <div class="row col-md-6">
+        <div class="col-md-4 col-4"><h5>Joined</h5></div>
+        <div class="col-md-8 col-8">{!! format_date($user->created_at, false) !!} ({{ $user->created_at->diffForHumans() }})</div>
+    </div>
+    <div class="row col-md-6">
+        <div class="col-md-4 col-4"><h5>Status</h5></div>
+        <div class="col-md-8 col-8">{!! $user->rank->displayName !!} {!! add_help($user->rank->parsed_description) !!}</div>
+    </div>
+    @if($user->birthdayDisplay && isset($user->birthday))
+        <div class="row col-md-6">
+            <div class="col-md-4 col-4"><h5>Birthday</h5></div>
+            <div class="col-md-8 col-8">{!! $user->birthdayDisplay !!}</div>
+        </div>
+    @endif
+    @if($user_enabled && isset($user->home_id))
+        <div class="row col-md-6">
+            <div class="col-md-4 col-4"><h5>Home</h5></div>
+            <div class="col-md-8 col-8">{!! $user->home ? $user->home->fullDisplayName : '-Deleted Location-' !!}</div>
+        </div>
+    @endif
+    @if($user_factions_enabled && isset($user->faction_id))
+        <div class="row col-md-6">
+            <div class="col-md-4 col-4"><h5>Faction</h5></div>
+            <div class="col-md-8 col-8">{!! $user->faction ? $user->faction->fullDisplayName : '-Deleted Faction-' !!}{!! $user->factionRank ? ' ('.$user->factionRank->name.')' : null !!}</div>
+        </div>
+    @endif
 </div>
 
 @if($user->is_deactivated)
