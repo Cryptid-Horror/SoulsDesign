36055e0c324b62cd49f072e99d983d2a6cab75f7
Cryptid-Horror <matterintothevoid@gmail.com>
2022-04-28 18:38:07 -0500

    Changed some formatting on genetics template

--- resources/views/design_guides/templates/_gene_template.blade.php
+++ resources/views/design_guides/templates/_gene_template.blade.php
@@ -23,7 +23,7 @@
         <div class="row">
             <div class="col-xl-9 mb-3">
                 <!-- Marking Name and Code -->
-                <h3>{{ $marking_name }}<center>{{ $marking_code }} <br> {{ $marking_type }}</center></h3>
+                <h3>{{ $marking_name }}<center>{{ $marking_code }}</h3> <br> <h2>{{ $marking_type }}</center></h2>
 
                 <!-- Marking Description -->
                 {{ $marking_desc }}
