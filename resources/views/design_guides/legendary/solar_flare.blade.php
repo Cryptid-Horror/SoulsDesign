2434a2781ac2e894d3cd1421b0be9cdb0f8bdf94
Cryptid-Horror <matterintothevoid@gmail.com>
2022-06-02 17:49:38 -0500

    Solar flare update.

--- resources/views/design_guides/legendary/solar_flare.blade.php
+++ resources/views/design_guides/legendary/solar_flare.blade.php
@@ -3,7 +3,7 @@
     $marking_name = 'Solar Flare';
     $marking_code = 'nSf/SfSf';
     $marking_type = 'Variable';
-    $marking_desc = "A marking that causes the horns, claws, or spikes on the dragon to glow and allows the horns to be any color.";
+    $marking_desc = "A marking that causes a brilliant wave color to cascade from a point on the character's body. Suggested by PlusBacon.";
     $layers_above_or_below = '';
     $layers_above = '';
     $layers_below = '';
@@ -45,31 +45,38 @@
         'solarflare_yes2',
         'solarflare_yes3',
         'solarflare_yes4',
+        'solarflare_yes5',
+        'solarflare_yes6',
         'solarflare_no1',
         'solarflare_no2',
         'solarflare_no5',
+        'solarflare_no6',
+        'solarflare_no7',
     ];
 
     // You can use html!
     $marking_can = [
-        'Is allowed up to a 12 point value and saturation point gradient difference inside the marking. This gradient may not blend into the base, or appear to blend into the base.',
-        'Solar flare is allowed to make the horns, spikes, and claws any color. The horns, claws, spikes can alternate in color and do not have to all be the same color.',
-        'Solar Flare can cause a gradient of colors based on if it is recessive or dominant.',
-        'Solar flare causes a gold, which can appear behind or in front of the horns, claws, and spikes.',
-        'Solar flare can effect only one, or all horns/claws/spikes, including one or the other horn or alternating spikes/etc.',
-
+        '<b>Value and Saturation Gradient Blend</b> = Yes; 12 Points.<br>
+        ♦ Solar flare should appear as a gradient with a minimum 3 to 4 colors blending into the base coat. <br>
+        ♦ Solar flare can be any color, affected by a color marking, or affected by duotone and tritone to give it more color options.<br>
+        ♦ Dark colors are allowed so long as there are at least 3 or 4 different colors present in the gradient.<br> 
+        ♦ An <i>optional</i> glow is allowed only <b>behind</b> the dragon. It may have a secondary glow spot to create a lens flare effect.',
     ];
 
     $marking_cannot = [
-        'Solar flare cannot have a metallic shine effect (It can have shading though)',
-        'Solar flare cannot obstruct too much of the design.',    
-        'Solar flare can ONLY affect the horns, claws, and spikes',
+        'There should be no metallic shine effect on the dragon.<br>
+        ♦ Mimicing another marking is not allowed. Be especially wary of mimicing stained or dipped by only have one color.<br> 
+        ♦ The <i>optional</i> glow must be behind the dragon and should not be on top of it. Neither should the lens flare effect. <br> 
+        ♦ Unlike Solar flares original design, it may not cause the horn, claws, or spikes to glow. However the <i>optional</i> glow may be behind the horns/etc.<br>
+        ♦ While you can use different shades of colors, there needs to be at least enough variation so that it is not just three or four shades of the same color. I.e. using a orange red, yellow red, red, and a more purple tone will be correct, while simply using four shades of red will not be correct.',
     ];
 
     $marking_must = [
-        'Recessive can allow the horns/spikes/claws to gradient up to four colors.',
-        'Dominant can allow the horns/spikes/claws gradient up to seven colors.',
-        'Solar Flare must have a glow.',
+        '<b>Recessive Coverage:</b> 40% of the body and from only a single "point."<br>
+        ♦ <b>Dominant Coverage:</b> 60% of the body and can be from two "points."<br>
+        ♦ <i>Points</i> Refer to one of the following parts of the character: Head, Front Legs, Back Legs, Wings, Tail. Front legs would count as one point, same as back legs and wings.<br>
+        ♦ In the case of <i>dominant</i> solar flare, two glows may be present <b>behind</b> the body with two lens flares also behind the body.<br>
+        ♦ Solar flare must have a minimum of 3 or 4 colors present in the marking, however it can up to 7 colors maximum.',
     ];
 
     // If left empty, the swatches section will not be shown
