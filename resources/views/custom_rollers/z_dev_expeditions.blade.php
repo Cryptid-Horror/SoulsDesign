47b196fc06307994f626751430e3a03f161be43c
Cryptid-Horror <matterintothevoid@gmail.com>
2022-06-24 17:51:18 -0500

    Moved some stuff again

--- resources/views/custom_rollers/z_dev_expeditions.blade.php
+++ resources/views/custom_rollers/z_dev_expeditions.blade.php
@@ -78,71 +78,65 @@
                     </select>
                 </form>
             </td>
-            <td>
-                <form id="modifiers" align="center">
-                    <label class="radioLabel"><input type="checkbox" id="barrely" name="barrely" value="barrely"
-                            data-toggle="tooltip" data-placement="top" title="Free rolls get less loot.">✦Free
-                        Roll?</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="hoardery" name="hoardery" value="hoardery"
-                            data-toggle="tooltip" data-placement="top" title="+1 item returned.">✦Hoarder Skill</label>
-                    <label class="radioLabel"><input type="checkbox" id="bagy" name="bagy" value="bagy"
-                            data-toggle="tooltip" data-placement="top"
-                            title="Basket (Forage/Cave) or Cooler (Hunt/Fish).">✦Container</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="mimicy" name="mimicy" value="mimicy"
-                            data-toggle="tooltip" data-placement="top"
-                            title="Hunt: Vulture || Fish: Reticulated Crocodile || Forage: Toucan || Cave: Dracobat">✦Extra
-                        Item Pet</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="charmy" name="charmy" value="charmy"
-                            data-toggle="tooltip" data-placement="top" title="Item Rarity Buff.">✦Charm</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="tamey" name="tamey" value="tamey"
-                            data-toggle="tooltip" data-placement="top" title="Item Rarity Buff">✦Morality Trial</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="Blue" name="Blue" value="Blue"
-                            data-toggle="tooltip" data-placement="top"
-                            title="Increase chance for Recipe Fragments">✦Blueprint</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="newty" name="newty" value="newty"
-                            data-toggle="tooltip" data-placement="top"
-                            title="Increase chance for Recipe Fragments">✦Newt Pet</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="souly" name="souly" value="souly"
-                            data-toggle="tooltip" data-placement="top" title="Decrease chance of injury">✦Spirit
-                        Trial</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="nofaily" name="nofaily" value="nofaily"
-                            data-toggle="tooltip" data-placement="top" title="You won't fail!">✦Pearl Necklace</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="AdeptY" name="AdeptY" value="AdeptY"
-                            data-toggle="tooltip" data-placement="top" title="Ensure location matches!">✦Adept
-                        Skill</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="famY" name="famY" value="famY"
-                            data-toggle="tooltip" data-placement="top"
-                            title="Hunt: Ursus || Fish: Otter|| Forage: Deer || Cave: Cane toad">✦Pass Pet</label>
-
-                    <label class="radioLabel"><input type="checkbox" id="localy" name="localy" value="localy"
-                            data-toggle="tooltip" data-placement="top"
-                            title="Vigilant: Frigid and Gloom || Aggressive: Scorched and Shimmering || Calm: Aether and Radiant || Sinister: All Locations">✦Temper
-                        Buff</label>
-                    </tr>
+            <div id="buttoncontainer" align="center">
+                <button class="button" onclick="roll()">roll</button>
+                <button class="button" onclick="clearForms()">reset</button>
+            </div>
+            <div id="output" align="left">
+                <div id="result"></div>
         </table>
-        </form>
-        </td>
-        <div id="buttoncontainer" align="center">
-            <button class="button" onclick="roll()">roll</button>
-            <button class="button" onclick="clearForms()">reset</button>
-        </div>
-        <div id="output" align="left">
-            <div id="result"></div>
-            </table>
 
-            <div id="cred">Copyright Souls Between 2019, All Rights Reserved.<br>
-                Version 2.0.0</div>
-        </div>
+        <form id="modifiers" align="center">
+            <label class="radioLabel"><input type="checkbox" id="barrely" name="barrely" value="barrely"
+                    data-toggle="tooltip" data-placement="top" title="Free rolls get less loot.">✦Free
+                Roll?</label>
+
+            <label class="radioLabel"><input type="checkbox" id="hoardery" name="hoardery" value="hoardery"
+                    data-toggle="tooltip" data-placement="top" title="+1 item returned.">✦Hoarder Skill</label>
+            <label class="radioLabel"><input type="checkbox" id="bagy" name="bagy" value="bagy" data-toggle="tooltip"
+                    data-placement="top" title="Basket (Forage/Cave) or Cooler (Hunt/Fish).">✦Container</label>
+
+            <label class="radioLabel"><input type="checkbox" id="mimicy" name="mimicy" value="mimicy"
+                    data-toggle="tooltip" data-placement="top"
+                    title="Hunt: Vulture || Fish: Reticulated Crocodile || Forage: Toucan || Cave: Dracobat">✦Extra
+                Item Pet</label>
+
+            <label class="radioLabel"><input type="checkbox" id="charmy" name="charmy" value="charmy"
+                    data-toggle="tooltip" data-placement="top" title="Item Rarity Buff.">✦Charm</label>
+
+            <label class="radioLabel"><input type="checkbox" id="tamey" name="tamey" value="tamey" data-toggle="tooltip"
+                    data-placement="top" title="Item Rarity Buff">✦Morality Trial</label>
+
+            <label class="radioLabel"><input type="checkbox" id="Blue" name="Blue" value="Blue" data-toggle="tooltip"
+                    data-placement="top" title="Increase chance for Recipe Fragments">✦Blueprint</label>
+
+            <label class="radioLabel"><input type="checkbox" id="newty" name="newty" value="newty" data-toggle="tooltip"
+                    data-placement="top" title="Increase chance for Recipe Fragments">✦Newt Pet</label>
+
+            <label class="radioLabel"><input type="checkbox" id="souly" name="souly" value="souly" data-toggle="tooltip"
+                    data-placement="top" title="Decrease chance of injury">✦Spirit
+                Trial</label>
+
+            <label class="radioLabel"><input type="checkbox" id="nofaily" name="nofaily" value="nofaily"
+                    data-toggle="tooltip" data-placement="top" title="You won't fail!">✦Pearl Necklace</label>
+
+            <label class="radioLabel"><input type="checkbox" id="AdeptY" name="AdeptY" value="AdeptY"
+                    data-toggle="tooltip" data-placement="top" title="Ensure location matches!">✦Adept
+                Skill</label>
+
+            <label class="radioLabel"><input type="checkbox" id="famY" name="famY" value="famY" data-toggle="tooltip"
+                    data-placement="top" title="Hunt: Ursus || Fish: Otter|| Forage: Deer || Cave: Cane toad">✦Pass
+                Pet</label>
+
+            <label class="radioLabel"><input type="checkbox" id="localy" name="localy" value="localy"
+                    data-toggle="tooltip" data-placement="top"
+                    title="Vigilant: Frigid and Gloom || Aggressive: Scorched and Shimmering || Calm: Aether and Radiant || Sinister: All Locations">✦Temper
+                Buff</label>
+        </form>
+        </table>
+        <div id="cred">Copyright Souls Between 2019, All Rights Reserved.<br>
+            Version 2.0.0</div>
+    </div>
     </div>
     <br>
 
