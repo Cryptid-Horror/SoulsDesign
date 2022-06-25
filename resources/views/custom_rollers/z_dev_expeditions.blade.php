71fc46d678136f693d1150e1f2caa8946456ed44
Cryptid-Horror <matterintothevoid@gmail.com>
2022-06-24 20:26:30 -0500

    made text area div class

--- resources/views/custom_rollers/z_dev_expeditions.blade.php
+++ resources/views/custom_rollers/z_dev_expeditions.blade.php
@@ -152,7 +152,7 @@
 	<button class="button" onclick="roll()">roll</button>
 	<button class="button" onclick="clearForms()">reset</button>
 </div>
-            <div rows= "10" cols= "40" id="result" onClick= "this.select();"></div><br><br>
+            <div class="textarea" rows= "10" cols= "40" id="result" onClick= "this.select();"></div><br><br>
         </div>
         
         <br><br>
