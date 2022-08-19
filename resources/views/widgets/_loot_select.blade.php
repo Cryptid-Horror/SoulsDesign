e38f10a596d532ab27ba1d27b601ac0dcfb7171f
Cryptid-Horror <matterintothevoid@gmail.com>
2022-08-19 15:48:22 -0500

    Bug with show recipes?

--- resources/views/widgets/_loot_select.blade.php
+++ resources/views/widgets/_loot_select.blade.php
@@ -47,7 +47,7 @@
                             {!! Form::select('rewardable_id[]', $tables, $loot->rewardable_id, ['class' => 'form-control table-select selectize', 'placeholder' => 'Select Loot Table']) !!}
                         @elseif($showRaffles && $loot->rewardable_type == 'Raffle')
                             {!! Form::select('rewardable_id[]', $raffles, $loot->rewardable_id, ['class' => 'form-control raffle-select selectize', 'placeholder' => 'Select Raffle']) !!}
-                        @elseif($showRecipes && $loot->rewardable_type == 'Recipe')
+                            @elseif($showRecipes && $loot->rewardable_type == 'Recipe')
                             {!! Form::select('rewardable_id[]', $recipes, $loot->rewardable_id, ['class' => 'form-control recipe-select selectize', 'placeholder' => 'Select Recipe']) !!}
                         @endif
                     </td>
