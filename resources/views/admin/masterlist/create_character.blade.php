629c50680136cb66be0b493d9008968a05ef03af
Cryptid-Horror <matterintothevoid@gmail.com>
2022-08-31 11:31:57 -0500

    Collapsed accordian layer 1 only

--- resources/views/admin/masterlist/create_character.blade.php
+++ resources/views/admin/masterlist/create_character.blade.php
@@ -408,12 +408,12 @@
     <div class="card-header" id="headingOne">
       <h5 class="mb-0">
         <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Rites and Activities
+            Rites and Activities (Select to show)
         </button>
       </h5>
     </div>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
       <div class="card-body">
       <h3>Rites and Activities</h3>
 
